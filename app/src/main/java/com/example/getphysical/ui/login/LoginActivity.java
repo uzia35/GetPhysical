eb9180246841dce3348fcd6e2cb04967700de83e
Usman <usmana@vt.edu>
2020-09-17 00:40:32 -0400

    master: new commit working front end for login and register

---
+++ app/src/main/java/com/example/getphysical/ui/login/LoginActivity.java
@@ -0,0 +1,106 @@
+package com.example.getphysical.ui.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.example.getphysical.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient mGoogleSignInClient;
+    private int GOOGLE_SIGN_IN_REQUEST_ID = 1;
+
+    private OnClickListener googleSignInListener = v -> {
+        Log.w("INFO", "calling signing in with google");
+        signInWithGoogle();
+    };
+
+    private OnClickListener universalSignInListener = v -> {
+        Log.w("INFO", "calling signing in with email/pass");
+        signInWithEmailAndPass();
+    };
+
+    private OnClickListener registerListener = v -> {
+        Log.w("INFO", "switching view to register");
+        setContentView(R.layout.activity_register);
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        try {
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
+        findViewById(R.id.google_sign_in_button).setOnClickListener(googleSignInListener);
+        findViewById(R.id.universal_sign_in_button).setOnClickListener(universalSignInListener);
+        findViewById(R.id.register).setOnClickListener(registerListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        Amplify.Auth.fetchAuthSession(
+                result -> Log.i("AmplifyQuickstart", result.toString()),
+                error -> Log.e("AmplifyQuickstart", error.toString())
+        );
+        //updateUI(account);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == GOOGLE_SIGN_IN_REQUEST_ID) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.w("INFO", "signInResult:success code= " + account.getDisplayName());
+            //updateUI(account);
+        } catch (ApiException e) {
+            Log.w("INFO", "signInResult:failed code= " + e.getStatusCode());
+            //updateUI(null);
+        }
+    }
+
+    private void signInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN_REQUEST_ID);
+    }
+
+    private void signInWithEmailAndPass() {
+
+
+    }
+
+}
